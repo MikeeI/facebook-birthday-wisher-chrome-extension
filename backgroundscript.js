fa53903aa8b7946c48dae462cc640df7e2de4020
Amith <mkamithkumar@gmail.com>
2014-01-27 22:20:42 +0530

    content scripts added

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,10 +1,14 @@
 function show(){
-	document.getElementById("birthday_reminders_link").click();
+	// Listen for the content script to send a message to the background page.
+	chrome.extension.onRequest.addListener(onRequest);
+	//document.getElementById("birthday_reminders_link").click();
 	var div = document.createElement("div");
-	var birthdayNames = document.getElementsByClassName('fbRemindersSubtitleActionList fsm fwn fcg');
-	for(var c = 0; c < birthdayNames.length; c++){
-		var nameTags = birthdayNames[c].getElementsByTagName('a');
-		var name = nameTags[0].innerHTML;
+	//var birthdayNames = document.getElementsByClassName('fbRemindersSubtitleActionList fsm fwn fcg');
+	//for(var c = 0; c < birthdayNames.length; c++){
+	for(var c = 0; c < 4; c++){
+		//var nameTags = birthdayNames[c].getElementsByTagName('a');
+		//var name = nameTags[0].innerHTML;
+		var name = "Amith";
 		//dynamically create elements
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
